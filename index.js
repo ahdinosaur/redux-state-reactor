66cc906a8135e87a96973d2f81748261a27df9d0
Michael Williams <michael.williams@enspiral.com>
2018-05-23 18:38:16 +1200

    initial implementation with happy test

--- index.js
+++ index.js
@@ -2,13 +2,38 @@ module.exports = {
   middleware: createMiddleware
 }
 
+// https://redux.js.org/advanced/middleware
 function createMiddleware (reactors) {
   return function middleware (store) {
     return function onNext (next) {
       return function onAction (action) {
-        console.log('middleware action', action)
+        // dispatch incoming action
         next(action)
+
+        // open question:
+        //     get state once for all reactors
+        // OR  get state for each reactor
+        // ?
+
+        // apply all reactors
+        reactors.forEach(applyReactor(store))
       }
     }
+  }
+}
+
+function applyReactor (store) {
+  return function (reactor) {
+    // get current state
+    const state = store.getState()
+
+    // get reaction from current state
+    const reaction = reactor(state)
+
+    // if no reaction
+    if (reaction == null || reaction == false) return
+
+    // otherwise dispatch (re)action
+    store.dispatch(reaction)
   }
 }
--- test/index.js
+++ test/index.js
@@ -10,16 +10,58 @@ test('redux-state-reactor', function (t) {
 })
 
 test('runs reactors after every action', function (t) {
-  const ACTION = { type: 'test' }
+  const calls = 3
 
-  const reducer = (state = {}, action) => {
-    if (action === ACTION) return { test: true }
+  const ACTION = { type: 'test' }
+  const STATE = {}
+  var called = 0
+
+  const reducer = (state = STATE, action) => state
+
+  const reactor = state => {
+    t.is(state, STATE)
+    called++
+    return false
+  }
+
+  const stateReactors = [
+    reactor
+  ]
+
+  const store = createStore(
+    reducer,
+    applyMiddleware(
+      stateReactorMiddleware(stateReactors)
+    )
+  )
+  
+  for (var i = 0; i < calls; i++) {
+    store.dispatch(ACTION)
+  }
+
+  t.is(called, calls)
+})
+
+// test('reactor can dispatch from initial state', function (t) {
+
+test('reactor can return action', function (t) {
+  const ACTION_ONE = { type: 'one' }
+  const ACTION_TWO = { type: 'two' }
+  const STATE_ONE = {}
+  const STATE_TWO = {}
+  const STATE_THREE = {}
+
+  var called = 0
+
+  const reducer = (state = STATE_ONE, action) => {
+    if (action === ACTION_ONE) return STATE_TWO
+    if (action === ACTION_TWO) return STATE_THREE
     return state
   }
 
   const reactor = state => {
-    console.log('reactor', state)
-    if (!state.test) return ACTION
+    called++
+    if (state === STATE_TWO) return ACTION_TWO
     return false
   }
 
@@ -34,13 +76,11 @@ test('runs reactors after every action', function (t) {
     )
   )
 
-  var state = store.getState()
-  console.log('before state', state)
+  store.dispatch(ACTION_ONE)
 
-  store.dispatch(ACTION)
-
-  state = store.getState()
-  console.log('after state', state)
+  const state = store.getState()
+  t.is(state, STATE_THREE)
+  t.is(called, 3) // once for each state
 })
 
 // test('race condition')
